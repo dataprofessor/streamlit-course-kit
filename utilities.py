320b9f1e484568c721f29d69b603741f97b01e83
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-03-22 08:49:58 +0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
 def load_css():
-    with open("style.css") as f:
-        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+  with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def st_image(link_input, image_input):
+  return st.markdown(f'''
+    <center>
+    <a href="{link_input}">
+      <img src="{image_input}" width="80%">
+    </a>
+    </center>''', unsafe_allow_html=True)
