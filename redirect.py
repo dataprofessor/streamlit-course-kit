7c8b3751fc805b61766cfbe5cb953c49cf36b212
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-10-31 20:31:06 +0700

    Update redirect.py

--- redirect.py
+++ redirect.py
@@ -6,7 +6,7 @@ st.write('https://course-kit.streamlit.app/')
 
 def nav_to(url):
     nav_script = """
-        <meta http-equiv="refresh" content="0; url='%s'">
+        <meta http-equiv="refresh" content="1; url='%s'">
     """ % (url)
     st.write(nav_script, unsafe_allow_html=True)
 
